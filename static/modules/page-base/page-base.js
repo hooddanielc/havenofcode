ff84dcd53a9483aefa147b1cdb04ece13d5c763d
hooddanielc <hood.danielc@gmail.com>
2014-04-04 13:09:25 -0700

    Added google analytics

--- static/modules/page-base/page-base.js
+++ static/modules/page-base/page-base.js
@@ -26,6 +26,14 @@ app.modules.PageBaseView = Backbone.View.extend({
     'click .fb-login': 'login',
     'click .fb-logout': 'logout'
   },
+  initialize: function() {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-27227565-1', 'auto');
+    ga('send', 'pageview');
+  },
   login: function(e) {
     e.preventDefault();
     window.location = '/oauth_login.php';
