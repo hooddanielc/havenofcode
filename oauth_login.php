ee265662b6ffed288d877cb677e67ffd54436258
Daniel Hood <hood.danielc@gmail.com>
2017-12-24 00:15:38 -0800

    Fix login for both havenofcode.com and www.havenofcode.com

--- oauth_login.php
+++ oauth_login.php
@@ -1,4 +1,18 @@
 <?php
+  function get_redirect_domain() {
+    // prod urls should always contain 'www' in domain
+    $parts = explode('.', $_SERVER['SERVER_NAME']);
+    if (count($parts) == 2 && $parts[count($parts) - 1] == 'com') {
+      return 'www.' . $_SERVER['SERVER_NAME'];
+    }
+    return $_SERVER['SERVER_NAME'];
+  }
+
+  if (get_redirect_domain() != $_SERVER['SERVER_NAME']) {
+    header('Location: ' . 'http://' . get_redirect_domain() . $_SERVER['REQUEST_URI']);
+    die();
+  }
+
   include 'config.php';
   $state = base64_encode(rand());
   $client_id = GITHUB_CLIENT;
